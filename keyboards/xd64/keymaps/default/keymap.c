ec1e23586c70e0ef1edb75b8159ce9884a83d346
sadaoikebe <58199305+sadaoikebe@users.noreply.github.com>
2020-01-18 02:36:40 +0900

    added delete key on layer 1

--- keyboards/xd64/keymaps/default/keymap.c
+++ keyboards/xd64/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 1: Function Layer
   LAYOUT_64_ansi_split_noarrow(
-      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_F13, \
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR, KC_NO,   KC_BRK,  KC_NO,  \
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_INS,  KC_DEL,  KC_NO,           \
       KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,                    \
