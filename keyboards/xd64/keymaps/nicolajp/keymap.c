29bcf71e07730882ff917b9d24a6272a55ca5bde
sadaoikebe <58199305+sadaoikebe@users.noreply.github.com>
2020-02-05 00:57:42 +0900

    compatibility with Android

--- keyboards/xd64/keymaps/nicolajp/keymap.c
+++ keyboards/xd64/keymaps/nicolajp/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,           \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                    \
-      MO(_FUNC),   KC_LGUI, KC_LALT,                          KC_SPC,  KC_SPC,                   KC_RALT, KC_RGUI,  KC_APP,  KC_RCTL  ),
+      MO(_FUNC),   KC_LGUI, KC_LALT,                      KC_SPC,  KC_SPC,                   KC_RALT, KC_RGUI, LT(_FUNC, KC_APP), KC_RCTL ),
 
 // NICOLA親指シフト
   // デフォルトレイヤーに関係なくQWERTYで
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  NG_Q,    NG_W,    NG_E,    NG_R,   NG_T,     NG_Y,    NG_U,   NG_I,    NG_O,    NG_P,    NG_LBRC,  NG_RBRC, NG_BSLS, \
       KC_LCTL, NG_A,    NG_S,    NG_D,    NG_F,   NG_G,     NG_H,    NG_J,   NG_K,    NG_L,    NG_SCLN, NG_QUOT,  KC_ENT,           \
       KC_LSFT, NG_Z,    NG_X,    NG_C,    NG_V,   NG_B,     NG_N,    NG_M,   NG_COMM, NG_DOT,  NG_SLSH, KC_RSFT,                    \
-      MO(_FUNC),   KC_LGUI, KC_LALT,                  NG_SHFTR, NG_SHFTL,                      KC_RALT, KC_RGUI,  KC_APP,  KC_RCTL  ),
+      MO(_FUNC), KC_LGUI, KC_LALT,                          NG_SHFTR, NG_SHFTL,                KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS  ),
 
   // Function Layer
   [_FUNC] = LAYOUT_64_ansi_split_noarrow( \
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_HOME,  KC_PGDN, KC_PGUP, KC_END,   KC_PSCR, KC_NO,   KC_BRK,  KC_NO,  \
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_INS,  KC_DEL,  KC_ENT,          \
       KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,                    \
-      MO(_FUNC),   KC_NO,   KC_NO,                         KC_KANA2, KC_EISU,                   KC_NO,   KC_NO,   KC_NO,   KC_NO   )
+      MO(_FUNC), KC_NO, LSFT(KC_SPC),                      KC_KANA2, KC_EISU,                    KC_MHEN, KC_TRNS, KC_TRNS, KC_TRNS )
 
 };
 
@@ -107,7 +107,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KC_EISU:
       if (record->event.pressed) {
         // NICOLA親指シフト
-        send_string(SS_TAP(X_MHEN));
+        send_string(SS_TAP(X_MHEN) SS_TAP(X_MHEN) SS_TAP(X_MHEN));
         nicola_off();
         // NICOLA親指シフト
         update_led();
