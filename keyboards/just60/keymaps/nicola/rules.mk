77594b3a858d2eb138b480e3244b5ab1a98862e8
sadaoikebe <58199305+sadaoikebe@users.noreply.github.com>
2020-03-04 00:04:54 +0900

    remove older nicola layout, moving to 106key

--- keyboards/just60/keymaps/nicola/rules.mk
+++
@@ -1,7 +0,0 @@
-# If you want to change the display of OLED, you need to change here
-SRC +=  nicola.c \
-        key_duration.c
-
-# 16-bit timer (TIMER1) を使用したタイムアウト
-# 一定時間経過すると、key down 状態のままでもキーが出力される
-OPT_DEFS = -DTIMEOUT_INTERRUPT
