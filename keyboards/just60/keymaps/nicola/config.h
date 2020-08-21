4c83895bca31bd3ae1eac04f80de6d88b90c30fa
Sadao Ikebe <58199305+sadaoikebe@users.noreply.github.com>
2020-08-22 01:27:34 +0900

    converge build rules

--- keyboards/just60/keymaps/nicola/config.h
+++ keyboards/just60/keymaps/nicola/config.h
@@ -24,3 +24,17 @@
 
 #undef PRODUCT
 #define PRODUCT         Just60 NICOLA Oyayubi Shift
+
+#define LAYOUT_62( \
+      K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K48  , \
+      K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D  , \
+      K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,          K2D  , \
+      K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,          K3C,          \
+      K40,   K41,   K42,          K44,   K45,                        K49,   K4A,          K4C,   K4D    \
+) { \
+    { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C, KC_NO }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D }, \
+    { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B, KC_NO,   K2D }, \
+    { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A, KC_NO,   K3C, KC_NO }, \
+    { K40,   K41,   K42,   KC_NO, K44,   K45, KC_NO, KC_NO,   K48,   K49,   K4A, KC_NO,   K4C,   K4D }, \
+}
--- keyboards/just60/keymaps/nicola/rules.mk
+++ keyboards/just60/keymaps/nicola/rules.mk
@@ -1,9 +1,6 @@
 BOOTLOADER = qmk-dfu
 
 # Build Options
-# NICOLA key down timre requires TIMER1 so Backlight LED must be turned off
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
