ff80906d779556f4c37024117dca98ad1eeb5893
Sadao Ikebe <58199305+sadaoikebe@users.noreply.github.com>
2020-08-22 01:30:58 +0900

    rollback to original matrix config

--- keyboards/just60/just60.h
+++ keyboards/just60/just60.h
@@ -37,5 +37,5 @@
     { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D }, \
     { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B, KC_NO,   K2D }, \
     { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A, KC_NO,   K3C, KC_NO }, \
-    { K40,   K41,   K42,   KC_NO, K44,   K45, KC_NO, KC_NO,   K48,   K49,   K4A, KC_NO,   K4C,   K4D }, \
+    { K40,   K41,   K42,   KC_NO, K44,   K45, KC_NO,   K47,   K48,   K49,   K4A,   K4B,   K4C,   K4D }, \
 }
