d9a2431db1164c8209e820857003811a3832369e
Sadao Ikebe <58199305+sadaoikebe@users.noreply.github.com>
2020-08-23 21:11:17 +0900

    add nicola mode indicator LED

--- keyboards/xd60/keymaps/nicola/rules.mk
+++ keyboards/xd60/keymaps/nicola/rules.mk
@@ -3,5 +3,5 @@ BOOTLOADER = qmk-dfu
 # Build Options
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
