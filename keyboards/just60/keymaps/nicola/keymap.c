1bbbdc78ea80e9d8cdfe806322348ad3bf7e28af
Ikebe Sadao <58199305+sadaoikebe@users.noreply.github.com>
2021-04-18 22:28:27 +0900

    removed jis to ansi conversion as kbd101 layer driver recognizes ime on/off

--- keyboards/just60/keymaps/nicola/keymap.c
+++ keyboards/just60/keymaps/nicola/keymap.c
@@ -22,7 +22,6 @@
 #include "bootloader.h"
 
 #include "nicola.h" // NICOLA親指シフト
-#include "jtu.h"    // JIS keyboard on ANSI layout hardware
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -37,15 +36,15 @@ enum keymap_layers {
 };
 
 enum custom_keycodes {
-  KC_EISU = JTU_SAFE_RANGE,
-  KC_KANA2
+  KC_EISU = NG_SAFE_RANGE,
+  KC_KANA2,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_62( \
-      KC_ESC,  KC_1,    JU_2,    KC_3,    KC_4,   KC_5,   JU_6,    JU_7,   JU_8,    JU_9,    JU_0,    JU_MINS,  JU_EQL,  KC_BSPC, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    JU_LBRC,  JU_RBRC, JU_BSLS, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    JU_SCLN, JU_QUOT,  KC_ENT,           \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,           \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                    \
       MO(_FUNC),   KC_LGUI, KC_LALT,                      KC_SPC,  KC_SPC,                   KC_RALT, KC_RGUI, LT(_FUNC, KC_APP), KC_RCTL ),
 
@@ -60,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Function Layer
   [_FUNC] = LAYOUT_62( \
-      JU_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_HOME,  KC_PGDN, KC_PGUP, KC_END,   KC_PSCR, KC_TRNS, KC_BRK,  KC_TRNS,\
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_INS,  KC_DEL,  KC_ENT,          \
       KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_INT1, KC_TRNS,                  \
@@ -107,10 +106,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (a == false) return false;
   // NICOLA親指シフト
 
-  bool continue_process = process_jtu(keycode, record);
-  if (continue_process == false) {
-    return false;
-  }
+// as kbd101 layer driver on windows recognizes the IME on/off key, we no longer need to disguise as 106 keyboard
+//   bool continue_process = process_jtu(keycode, record);
+//   if (continue_process == false) {
+//     return false;
+//   }
 
   return true;
 }
--- keyboards/just68/keymaps/nicola/keymap.c
+++ keyboards/just68/keymaps/nicola/keymap.c
@@ -22,7 +22,6 @@
 #include "bootloader.h"
 
 #include "nicola.h" // NICOLA親指シフト
-#include "jtu.h"    // JIS keyboard on ANSI layout hardware
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -37,34 +36,34 @@ enum keymap_layers {
 };
 
 enum custom_keycodes {
-  KC_EISU = JTU_SAFE_RANGE,
+  KC_EISU = NG_SAFE_RANGE,
   KC_KANA2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
-      KC_ESC,  KC_1,    JU_2,    KC_3,    KC_4,   KC_5,   JU_6,    JU_7,   JU_8,    JU_9,    JU_0,    JU_MINS,  JU_EQL,  KC_BSPC, KC_HOME, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    JU_LBRC,  JU_RBRC, JU_BSLS, KC_PGUP, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    JU_SCLN, JU_QUOT,  KC_ENT,           KC_PGDN, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,           KC_UP,   KC_END,  \
-      MO(_FUNC),   KC_LGUI, KC_LALT,                      KC_SPC,  KC_SPC, KC_RALT, KC_RGUI, LT(_FUNC, KC_APP), KC_LEFT, KC_DOWN, KC_RGHT ),
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_NO,  KC_BSPC, KC_HOME, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,          KC_BSLS, KC_PGUP, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,                    KC_PGDN, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                    KC_UP,   KC_END,  \
+      MO(_FUNC),   KC_LGUI, KC_LALT,                      KC_SPC,  KC_SPC, KC_RALT, KC_RGUI, LT(_FUNC, KC_APP), KC_LEFT,          KC_DOWN, KC_RGHT ),
 
 // NICOLA親指シフト
   // デフォルトレイヤーに関係なくQWERTYで
     [_NICOLA] = LAYOUT( \
-      KC_ESC,  NG_1,    NG_2,    NG_3,    NG_4,   NG_5,     NG_6,    NG_7,   NG_8,    NG_9,    NG_0,    NG_MINS,  NG_EQL,  KC_BSPC, KC_TRNS, \
-      KC_TAB,  NG_Q,    NG_W,    NG_E,    NG_R,   NG_T,     NG_Y,    NG_U,   NG_I,    NG_O,    NG_P,    NG_LBRC,  NG_RBRC, NG_BSLS, KC_TRNS, \
-      KC_LCTL, NG_A,    NG_S,    NG_D,    NG_F,   NG_G,     NG_H,    NG_J,   NG_K,    NG_L,    NG_SCLN, NG_QUOT,  KC_ENT,           KC_TRNS, \
-      KC_LSFT, NG_Z,    NG_X,    NG_C,    NG_V,   NG_B,     NG_N,    NG_M,   NG_COMM, NG_DOT,  NG_SLSH, KC_RSFT,           KC_TRNS, KC_TRNS, \
-      MO(_FUNC), KC_LGUI, KC_LALT,                         NG_SHFTL, NG_SHFTR,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
+      KC_ESC,  NG_1,    NG_2,    NG_3,    NG_4,   NG_5,     NG_6,    NG_7,   NG_8,    NG_9,    NG_0,    NG_MINS,  NG_EQL,  KC_TRNS, KC_BSPC, KC_TRNS, \
+      KC_TAB,  NG_Q,    NG_W,    NG_E,    NG_R,   NG_T,     NG_Y,    NG_U,   NG_I,    NG_O,    NG_P,    NG_LBRC,  NG_RBRC,          NG_BSLS, KC_TRNS, \
+      KC_LCTL, NG_A,    NG_S,    NG_D,    NG_F,   NG_G,     NG_H,    NG_J,   NG_K,    NG_L,    NG_SCLN, NG_QUOT,  KC_ENT,                    KC_TRNS, \
+      KC_LSFT, NG_Z,    NG_X,    NG_C,    NG_V,   NG_B,     NG_N,    NG_M,   NG_COMM, NG_DOT,  NG_SLSH, KC_RSFT,                    KC_TRNS, KC_TRNS, \
+      MO(_FUNC), KC_LGUI, KC_LALT,                         NG_SHFTL, NG_SHFTR,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS ),
 
   // Function Layer
   [_FUNC] = LAYOUT( \
-      JU_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_TRNS, \
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_HOME,  KC_PGDN, KC_PGUP, KC_END,   KC_PSCR, KC_TRNS, KC_BRK,  KC_TRNS, KC_TRNS, \
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_INS,  KC_DEL,  KC_ENT,           KC_TRNS, \
-      KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, \
-      MO(_FUNC), KC_TRNS, KC_TRNS,                         KC_EISU,  KC_KANA2,         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS )
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12, KC_TRNS, KC_DEL,  KC_TRNS, \
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_HOME,  KC_PGDN, KC_PGUP, KC_END,   KC_PSCR, KC_TRNS, KC_BRK,          KC_TRNS, KC_TRNS, \
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_INS,  KC_DEL,  KC_ENT,                   KC_TRNS, \
+      KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,                  KC_TRNS, KC_TRNS, \
+      MO(_FUNC), KC_TRNS, KC_TRNS,                         KC_EISU,  KC_KANA2,         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS )
 };
 
 void matrix_init_user(void) {
@@ -107,10 +106,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (a == false) return false;
   // NICOLA親指シフト
 
-  bool continue_process = process_jtu(keycode, record);
-  if (continue_process == false) {
-    return false;
-  }
+// as kbd101 layer driver on windows recognizes the IME on/off key, we no longer need to disguise as 106 keyboard
+//   bool continue_process = process_jtu(keycode, record);
+//   if (continue_process == false) {
+//     return false;
+//   }
 
   return true;
 }
