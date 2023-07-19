8df775870e7cd0535bde2c21729dc79b8b8d786b
Ikebe Sadao <58199305+sadaoikebe@users.noreply.github.com>
2023-07-19 22:08:02 +0900

    made ts52k keymap

--- keyboards/ts52k/keymaps/nicola/keymap.c
+++ keyboards/ts52k/keymaps/nicola/keymap.c
@@ -29,10 +29,9 @@
 // entirely and just use numbers.
 enum keymap_layers {
   _QWERTY,
-// NICOLA親指シフト
-  _NICOLA, // NICOLA親指シフト入力レイヤー
-// NICOLA親指シフト
-  _FUNC
+  _NICOLA,
+  _FUNC,
+  _FUNC2
 };
 
 enum custom_keycodes {
@@ -42,29 +41,28 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS, KC_PGUP, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,  KC_PGDN, \
-      KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,            \
-      MO(_FUNC), KC_LGUI, KC_LALT,        KC_SPC,                  KC_SPC,          KC_RALT, KC_RGUI, LT(_FUNC, KC_APP), KC_LEFT, KC_DOWN, KC_RGHT ),
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_MINS,  KC_EQL,   KC_BSPC, KC_NO,      \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,  MO(_FUNC2), \
+      KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,           KC_UP,      \
+      MO(_FUNC), KC_LGUI, KC_LALT,        KC_SPC,                  KC_SPC,          KC_RALT, KC_RGUI, LT(_FUNC2, KC_APP), KC_LEFT, KC_DOWN, KC_RGHT ),
 
-// NICOLA親指シフト
-  // デフォルトレイヤーに関係なくQWERTYで
     [_NICOLA] = LAYOUT( \
       KC_TAB,  NG_Q,    NG_W,    NG_E,    NG_R,   NG_T,   NG_Y,    NG_U,   NG_I,     NG_O,    NG_P,    NG_LBRC,  NG_RBRC, NG_BSLS, KC_TRNS, \
       KC_LCTL, NG_A,    NG_S,    NG_D,    NG_F,   NG_G,   NG_H,    NG_J,   NG_K,     NG_L,    NG_SCLN, NG_QUOT,           KC_ENT,  KC_TRNS, \
       KC_LSFT,          NG_Z,    NG_X,    NG_C,   NG_V,   NG_B,    NG_N,   NG_M,     NG_COMM, NG_DOT,  NG_SLSH,  KC_RSFT, KC_TRNS,          \
-      MO(_FUNC), KC_LGUI, KC_LALT,      NG_SHFTL,                NG_SHFTR,           KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
+      KC_TRNS, KC_LGUI, KC_LALT,      NG_SHFTL,                NG_SHFTR,             KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS ),
 
-  // Function Layer
   [_FUNC] = LAYOUT( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSPC, KC_TRNS, \
-      //KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,  KC_TRNS,
-      //KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   KC_PSCR, KC_TRNS, KC_BRK,    KC_TRNS, KC_TRNS,
-      //KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_INS,  KC_DEL,           KC_ENT,  KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV, KC_BRK, KC_INS,  KC_DEL,           KC_TRNS,  KC_TRNS,
-      KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,       KC_TRNS, KC_PGUP, \
-      //KC_LSFT,          KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_MINS,  KC_EQL, KC_TRNS, KC_PGUP, KC_TRNS,
-      MO(_FUNC), KC_TRNS, KC_TRNS,        KC_EISU,               KC_KANA2,       KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END )
+      KC_ESC,  S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_LBRC, KC_RBRC,  KC_BSLS, KC_TRNS, \
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_BSPC,           KC_TRNS, KC_TRNS, \
+      KC_TRNS,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,  KC_PGUP,          \
+      KC_TRNS, KC_TRNS, KC_TRNS,          KC_EISU,               KC_KANA2,             KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME,  KC_PGDN, KC_END ),
+
+  [_FUNC2] = LAYOUT( \
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_DEL,     KC_TRNS, \
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,    KC_TRNS, \
+      KC_TRNS,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_INS,  KC_PSCR, KC_BRK,  KC_TRNS,    S(KC_UP),            \
+      KC_TRNS, KC_TRNS, KC_TRNS,          KC_EISU,               KC_KANA2,             KC_TRNS, KC_TRNS, KC_TRNS, S(KC_LEFT), S(KC_DOWN), S(KC_RGHT))
 };
 
 void matrix_init_user(void) {
@@ -119,9 +117,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_DOWN);
+            if(layer_state_is(_FUNC) || layer_state_is(_FUNC2)) {
+                tap_code(KC_PGUP);
+            } else {
+                tap_code(KC_UP);
+            }
         } else {
-            tap_code(KC_UP);
+            if(layer_state_is(_FUNC) || layer_state_is(_FUNC2)) {
+                tap_code(KC_PGDN);
+            } else {
+                tap_code(KC_DOWN);
+            }
         }
     }
     return true;
